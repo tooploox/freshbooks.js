2067d2314b2086dc82aec2de7f45bcdcf20db69c
Marc Loney <marc@marcloney.com>
2012-05-22 22:27:14 +0800

    Fixing Invoices

--- examples/invoices.js
+++ examples/invoices.js
@@ -2,13 +2,13 @@ var FreshBooks = require('../lib/freshbooks'),
     freshbooks,
     invoice;
 
-freshbooks = new FreshBooks("https://{NAME}.freshbooks.com/api/2.1/xml-in","{TOKEN}");
+freshbooks = new FreshBooks("https://concisecreative.freshbooks.com/api/2.1/xml-in","0a6e5ed1a56732cebf7979ed7a3b2417");
+//freshbooks = new FreshBooks("https://{NAME}.freshbooks.com/api/2.1/xml-in","{TOKEN}");
 
-invoice = new freshbooks.invoice();
+invoice = freshbooks.Invoice();
 
 /* TODO: Do we want to get like tthis or invoice.get(100, function() { ... }); ?*/
-invoice.invoice_id=100;
-invoice.get(function() {
+invoice.get(169532, function() {
   console.log(this.number);
 });
 
